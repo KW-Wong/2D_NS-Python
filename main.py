b322b54e16596c45cf9f63c99bf7ef83e74bdf47
Kin Wing Wong <wong@student-net-vpn-4035.ethz.ch>
2019-04-01 20:36:37 +0200

    First Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+ Created 01.04.2019
+
+ A 2D Navier Stokes Solver in Python.
+
+ Governing Equation:
+ /[
+    
+ /]
+
+ Algorithm:
+    Projection Step Method by Chorin for pressure.
+
+"""
+
+# 2D Domain Creation
+LX = 2
+LY = 3
+nx = 90
+ny = 100
+
+
+
+
